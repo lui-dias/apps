a16ca8a8a01d27aa1d7146c7c062ebe767f69415
Luigi <luigidias@protonmail.com>
2024-07-28 21:49:43 -0300

    ...

--- wake/loaders/headlessCheckout.ts
+++ wake/loaders/headlessCheckout.ts
@@ -1,5 +1,5 @@
 import type { AppContext } from "../mod.ts";
 
 export default (_props: unknown, _req: Request, ctx: AppContext) => {
-  return ctx.useCustomCheckout;
+  return ctx.headlessCheckout;
 };
